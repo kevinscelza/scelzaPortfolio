3de6fc2d8859d0f8b3cb342b450efd2d2ad01ef5
kevinscelza <kevinscelza@gmail.com>
2023-07-17 14:02:40 -0700

    Created background for landing page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/sass/main.css";
+// import { BrowserRouter as Router, Routes, Route ,NavLink} from "react-router-dom";
+import LandingPage from "./screens/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+ 
+      <div className="App">
+      
+      <LandingPage></LandingPage>
+      </div>
+        
   );
 }
 
---
+++ src/screens/LandingPage.jsx
@@ -0,0 +1,17 @@
+
+
+function LandingPage() {
+  return (
+ <div className="bg-landing vh-100 container-fluid">
+   <div className="row">
+    <div className="col-6 d-flex vh-100 justify-content-center align-items-center">
+      
+        {/* <img src="./assets/images/me.png" alt="" className="img-fluid" /> */}
+    </div>
+    <div className="col-6"></div>
+   </div>
+ </div>
+  );
+}
+
+export default LandingPage;
