8ab0cbf3a9560d8221ef31ee3f0569047c7b9a49
kevinscelza <kevinscelza@gmail.com>
2023-07-19 11:32:49 -0700

    Added navigation , NavLinks to paginate

--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,4 +1,4 @@
-
+import {NavLink} from "react-router-dom";
 
 
 function LandingPage() {
@@ -10,7 +10,9 @@ function LandingPage() {
         <img src="./assets/images/me.png" alt=""   />
     </div>
     <div className="col-6 d-flex flex-column align-items-center  justify-content-around">
- 
+      <NavLink to='/live' className="text-grunge text-nav-lg text-light text-decoration-none">Live</NavLink>
+      <NavLink to='' className="text-grunge text-nav-lg  text-light text-decoration-none">Work</NavLink>
+      <NavLink  to='' className="text-grunge text-nav-lg  text-light text-decoration-none">Create</NavLink>
     </div>
    </div>
 
